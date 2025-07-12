df6c4c9916cee5ae119d9c924bda7294fb3fb23a
idaibin <bruce@idaibin.dev>
2025-07-13 00:17:45 +0800

    feat: add vercel config

---
+++ web/api/proxy.js
@@ -0,0 +1,13 @@
+import { createProxyMiddleware } from "http-proxy-middleware";
+
+// 只导出一个 default 函数即可
+export default function handler(req, res) {
+    const proxy = createProxyMiddleware({
+        target: process.env.API_TARGET, // 设置环境变量：真实后端地址
+        changeOrigin: true,
+        pathRewrite: {
+            "^/api": "", // 去掉前缀
+        },
+    });
+    return proxy(req, res);
+}

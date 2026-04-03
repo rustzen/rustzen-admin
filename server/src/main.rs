6ffc569e8d5e7af2e37c4f2379ad1a76edbb21a8
Bruce Dai <bruce@idaibin.dev>
2026-04-04 00:59:24 +0800

    refactor(infra): rename log module to logger

--- server/src/main.rs
+++ server/src/main.rs
@@ -4,14 +4,15 @@ mod infra;
 mod middleware;
 
 use crate::infra::app::run_server;
+use crate::infra::logger::init_logging;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // init log
-    tracing_subscriber::fmt().with_target(false).compact().init();
-
     // load env
     dotenvy::dotenv().ok();
+
+    // init log
+    let _logging = init_logging()?;
 
     // create server
     run_server().await?;
